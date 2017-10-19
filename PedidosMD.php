61f75862c3705844a2abf21dcbe8ce7e6d8c0b07
LizzNejer <32920831+LizzNejer@users.noreply.github.com>
2017-10-19 11:31:40 -0500

    Add files via upload

---
+++ PedidosMD.php
@@ -0,0 +1,34 @@
+<?php
+class PedidosMD
+{
+
+        function insertar($pedido) 
+	{
+
+		$id_con=mysqli_connect("localhost","root","");
+
+		mysqli_select_db($idcon, "Implementos");
+	    
+		$fecha=$pedido->getFecha();
+		$cod_articulo=$pedido->getCodarticulo();
+		$ID_cliente=$pedido->getIdcliente();
+
+		$query= "INSERT INTO PEDIDO (fecha, cod_articulo, ID_cliente )
+		VALUES('".$fecha."','".$cod_articulo."','".$ID_cliente."')";
+	
+		$result=mysqli_query($idcon,$query);
+
+		if($result)
+		{
+			echo "Registro insertado exitosamente";
+			foreach($result as $row)
+			{
+				echo $row['fecha','cod_articulo','ID_cliente']."<br>";
+			}
+		}		
+		else
+			die(mysqli_error($id_con));
+	}
+        
+}
+?>
