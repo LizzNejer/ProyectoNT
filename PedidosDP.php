78d18a70c7fad30e2a0b70570a88b1b0d7ad9088
LizzNejer <32920831+LizzNejer@users.noreply.github.com>
2017-11-12 16:24:55 -0500

    Add files via upload

---
+++ PedidosDP.php
@@ -0,0 +1,38 @@
+<?php
+
+class PedidosDP {
+    //put your code here
+    
+            var $fecha="";
+            var $cod_articulo="";
+            var $ID_cliente="";
+            
+            function setFecha($fecha)
+            {
+                $this->fecha=$fecha;
+            }
+            function getFecha()
+            {
+                return $this->fecha;
+            }
+            
+            function setCodArticulo($cod_articulo)
+            {
+                $this->cod_articulo=$cod_articulo;
+            }
+            function getCodArticulo()
+            {
+                return $this->cod_articulo;
+            }
+			
+			function setIDCliente($ID_cliente)
+            {
+                $this->ID_cliente=$ID_cliente;
+            }
+            function getIDCliente()
+            {
+                return $this->ID_cliente;
+            }
+            
+        }
+ ?>
