4650aa1cf44422183495618f6175c315c781cd0d
LizzNejer <32920831+LizzNejer@users.noreply.github.com>
2017-11-20 23:10:44 -0500

    Update FPagoMD.php

--- FPagoMD.php
+++ FPagoMD.php
@@ -28,6 +28,46 @@ class FPagoMD
 		else
 			die(mysqli_error($id_con));
 	}
+	
+	function consultar($nuevo){
+		
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+				$ID_Pedido = $nuevo->getID_Pedido();
+				
+				$resultado=$coleccion->find(['ID_Pedido'=>$ID_Pedido]);
+	
+				foreach($resultado as $row)
+				{
+					echo $row['ID_Pedido']."<br/>";
+				}
+		
+	}
+	function modificar($id){
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+		fpagoDP= new FPagoDP();
+		
+		$q = "UPDATE Implementos SET ID_Pedido="+pagodp->getID_Pedido() +"WHERE ID="+$id;
+		$rs = mysql_query($q);
+		if($rs == false) {
+			echo '<p>Error al modificar los campos en la tabla.</p>';
+		}else{
+			echo '<p>Los datos se han modificado correctamente.</p>';
+	}
+	
+	function eliminar($id)
+	{
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+		$q = "DELETE * FROM Implementos WHERE ID="+$id+;
+		$rs = mysql_query($q);
+		if($rs == false) {
+			echo '<p>Error al eliminar los campos en la tabla.</p>';
+		}else{
+			echo '<p>Los datos se han eliminado correctamente.</p>';
+		}
+	}
         
 }
 ?>
