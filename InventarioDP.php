02095c0323cfc7cfeaeb092c0bc5f57a2a025d6a
Edwin0313 <32930905+Edwin0313@users.noreply.github.com>
2017-11-20 22:09:55 -0500

    Add files via upload

---
+++ InventarioDP.php
@@ -0,0 +1,50 @@
+<?php
+    class InventarioDP
+    {
+        var $codigo;
+        var $nombre;
+        var $tipo;
+        var $estado;
+        var $valor;
+        function getCodigo() {
+            return $this->codigo;
+        }
+		
+        function getNombre() {
+            return $this->nombre;
+        }
+		
+        function getTipo() {
+            return $this->tipo;
+        }
+
+        function getEstado() {
+            return $this->estado;
+        }
+		
+        function getValor() {
+            return $this->valor;
+        }
+		
+        function setCodigo($codigo) {
+            $this->codigo = $codigo;
+        }
+
+        function setNombre($nombre) {
+            $this->nombre = $nombre;
+        }
+
+        function setTipo($tipo) {
+            $this->tipo = $tipo;
+        }
+		
+        function setEstado($estado) {
+            $this->estado = $estado;
+        }
+
+        function setValor($valor) {
+            $this->valor = $valor;
+        }
+
+    }
+?>
