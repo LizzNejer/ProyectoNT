20d7a9c6eab251d44ab123e15ada13ae4df0ef88
LizzNejer <32920831+LizzNejer@users.noreply.github.com>
2017-11-20 23:09:33 -0500

    Update PedidosMD.php

--- PedidosMD.php
+++ PedidosMD.php
@@ -29,6 +29,46 @@ class PedidosMD
 		else
 			die(mysqli_error($id_con));
 	}
+	
+	function consultar($id){
+		
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+				$ID_Pedido = $id->getID_Pedido();
+				
+				$resultado=$idcon->find(['ID_Pedido'=>$ID_Pedido]);
+	
+				foreach($resultado as $row)
+				{
+					echo $row['ID_Pedido']."<br/>";
+				}
+		
+	}
+	function modificar($id){
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+		pedidodp= new PedidosDP();
+		
+		$q = "UPDATE Implementos SET ID_Pedido="+pedidodp->getID_Pedido() +"WHERE ID_Pedido="+$id;
+		$rs = mysql_query($q);
+		if($rs == false) {
+			echo '<p>Error al modificar los campos en la tabla.</p>';
+		}else{
+			echo '<p>Los datos se han modificado correctamente.</p>';
+	}
+	
+	function eliminar($id)
+	{
+		$idcon=mysqli_connect("localhost","root","");
+		mysqli_select_db($idcon, "Implementos");
+		$q = "DELETE * FROM Implementos WHERE ID_Pedido="+$id+;
+		$rs = mysql_query($q);
+		if($rs == false) {
+			echo '<p>Error al eliminar los campos en la tabla.</p>';
+		}else{
+			echo '<p>Los datos se han eliminado correctamente.</p>';
+		}
+	}
         
 }
 ?>
