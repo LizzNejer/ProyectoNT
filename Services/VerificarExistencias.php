4ef8529a2472c8f04ea4d3ab8b867b04c8de7e89
Edwin0313 <32930905+Edwin0313@users.noreply.github.com>
2017-12-17 23:37:13 -0500

    Update VerificarExistencias.php

--- Services/VerificarExistencias.php
+++ Services/VerificarExistencias.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;//
 
-use App\Student;
+use App\Stock;
 
-class LibrosLN extends Controller
+class VerificarExistencias extends Controller
 {
 	public function show($consulta)
 	{
